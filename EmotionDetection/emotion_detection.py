ccfa57e502c8e0239f25d4a19a9f1e4cee17ba2c
manesh rajput <maneshrajput42@gmail.com>
2025-04-12 16:33:10 -0400

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,7 +1,15 @@
 import requests
 import json
-
 def emotion_detector(text_to_analyze):
+    if not text_to_analyze.strip():
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
@@ -11,16 +19,21 @@ def emotion_detector(text_to_analyze):
             "text": text_to_analyze
         }
     }
-
     response = requests.post(url, headers=headers, json=input_json)
+    if response.status_code != 200:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     response_dict = json.loads(response.text)
-
     # Extract emotions
     emotions = response_dict['emotionPredictions'][0]['emotion']
-
     # Find dominant emotion
     dominant_emotion = max(emotions, key=emotions.get)
-
     result = {
         'anger': emotions['anger'],
         'disgust': emotions['disgust'],
--- server.py
+++ server.py
@@ -9,9 +9,13 @@ def home():
 @app.route('/emotionDetector', methods=['GET', 'POST'])
 def detect_emotion():
     text_to_analyze = request.args.get('textToAnalyze')  # use the correct key name!
+    if not text_to_analyze:
+        return "Invalid text! Please try again!"
     result=emotion_detector(text_to_analyze)
     if not result :
         return "error",500
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
     formatted_result = (
         f"For the given statement, the system response is "
         f"'anger': {result['anger']}, "
